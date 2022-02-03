f6beb45c6547cf94c978439c4d6436cb521996de
john.marin <johnmarin@johhn.marin>
2022-02-03 16:46:24 -0500

    back

--- first_app/views.py
+++ first_app/views.py
@@ -8,3 +8,22 @@ from django.http import HttpResponse
 def index(request):
     print("ok")
     return HttpResponse("Hola mundo")
+
+
+def detail(request, question_id):
+
+    '''try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Pregunta no existe')'''
+
+    # question = get_object_or_404(Question, pk=question_id)
+
+    # return render(request,'detail.html',{'question':question,})
+
+    return HttpResponse("Detail %s" % question_id)
+
+def results(request, question_id):
+    # question = get_object_or_404(Question, pk=question_id)
+    # return render(request,'results.html',{'question':question,})
+    return HttpResponse("Results %s" % question_id)
